02c3557826b30b4fdfd4cd31b5508b9ad644125c
elonner <elilnnr5@gmail.com>
2023-06-05 14:41:02 -0700

    adds functionality to create skills and posts

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,7 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index"),
+    # home (for now)
+    path('', views.HomeFeedList.as_view(), name="home_feed_list"),
+    # sign up
     path('accounts/signup', views.signup, name='signup'),
+    # SKILL - create
     path('skills/create', views.SkillCreate.as_view(), name='skills_create'),
+    # POST - create
+    path('posts/create', views.PostCreate.as_view(), name='posts_create'),
 ]
