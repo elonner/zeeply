dc2cacaee868ae762a04cd34b10fad1926f5f12f
elonner <elilnnr5@gmail.com>
2023-06-05 17:26:13 -0700

    implement user profile detail page

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,13 +5,13 @@ urlpatterns = [
     # home (for now)
     path('', views.HomeFeedList.as_view(), name="home_feed_list"),
     # sign up
-    path('accounts/signup', views.signup, name='signup'),
+    path('accounts/signup/', views.signup, name='signup'),
     # PROFILE - create
-    path('profiles/create', views.ProfileCreate.as_view(), name='profiles_create'),
+    path('profiles/create/', views.ProfileCreate.as_view(), name='profiles_create'),
     # PROFILE - detail
-    path('profiles/<int:pk>', views.ProfileDetail.as_view(), name='profiles_detail'),
+    path('users/<int:pk>/', views.UserDetail.as_view(), name='users_detail'),
     # SKILL - create
-    path('skills/create', views.SkillCreate.as_view(), name='skills_create'),
+    path('skills/create/', views.SkillCreate.as_view(), name='skills_create'),
     # POST - create
-    path('posts/create', views.PostCreate.as_view(), name='posts_create'),
+    path('posts/create/', views.PostCreate.as_view(), name='posts_create'),
 ]
