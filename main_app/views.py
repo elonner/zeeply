eb84c39a6abab9d717c76b46c4ea61c34d3c37cb
elonner <elilnnr5@gmail.com>
2023-06-06 23:04:21 -0700

    adds follow/unfollow functionality, frames some template user flow

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,4 @@
+from typing import Any, Dict
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
@@ -43,7 +44,13 @@ class PostCreate(LoginRequiredMixin, CreateView):
         return super().form_valid(form)
     
 class PostDetail(LoginRequiredMixin, DetailView):
-  model = Post
+    model = Post
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        reviews = Review.objects.filter(post=self.kwargs.get('pk'))
+        context['reviews'] = reviews
+        return context
 
 #POST - CBV - UPDATE
 class PostUpdate(LoginRequiredMixin, UpdateView):
@@ -79,13 +86,27 @@ class ReviewDelete(LoginRequiredMixin, DeleteView):
     model = Review
     success_url = '/'
 
+def follow_user(request, user_id):
+    curr_user_prof = Profile.objects.get(user=request.user)
+    user_prof = Profile.objects.get(user=user_id)
+    curr_user_prof.following.add(User.objects.get(pk=user_id))
+    user_prof.followers.add(request.user)
+    return redirect('users_detail', user_id)
+
+def unfollow_user(request, user_id):
+    curr_user_prof = Profile.objects.get(user=request.user)
+    user_prof = Profile.objects.get(user=user_id)
+    curr_user_prof.following.remove(User.objects.get(pk=user_id))
+    user_prof.followers.remove(request.user)
+    return redirect('users_detail', user_id)
+
 def save_post(request, post_id):
     Post.objects.get(id=post_id).savedBy.add(request.user.id)
-    return redirect('home_feed_list')
+    return redirect('posts_detail', post_id)
 
 def unsave_post(request, post_id):
     Post.objects.get(id=post_id).savedBy.remove(request.user.id)
-    return redirect('home_feed_list')
+    return redirect('posts_detail', post_id)
 
 def signup(request):
     error_message = ''
